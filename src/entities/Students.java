b73df670c4b1de6cb4b9f4a4cfc045ee0c491ee5
Nadyne Madureira <madureira.nadyne@gmail.com>
2023-02-16 21:41:07 -0300

    FDM-M4-16-11 exercício de fixação Set

---
+++ src/entities/Students.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.util.Objects;
+
+public class Students {
+	
+	private String name;
+	private Long id;
+	
+	public Students(){
+		
+	}
+	
+	public Students(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Students other = (Students) obj;
+		return Objects.equals(id, other.id);
+	}
+}
